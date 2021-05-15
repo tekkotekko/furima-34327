b73d58edd145af060cfe679c1014e4b8c14cb4a8
tekkotekko <takekoe.s@gmail.com>
2021-05-15 23:23:21 +0900

    Basic 認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |user, pass|
+      user==ENV["FURIMA_AUTH_USER"] && pass==ENV["FURIMA_AUTH_PASSWORD"]
+    end
+  end
+
 end
