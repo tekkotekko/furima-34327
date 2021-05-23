6edc597dedf04ca433ce7c4c39a8fdbe553b63a6
tekkotekko <takekoe.s@gmail.com>
2021-05-23 21:39:05 +0900

    fee_calculation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,12 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-  
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
 
   private
 
   def item_params
+    params.require(:item).permit(:name, :image, :description, :category_id, :condition_id, :charge_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
 
 end
